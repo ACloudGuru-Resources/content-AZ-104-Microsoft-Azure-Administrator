9ed6c85d5ea855883a1d54d7acf330f8a9bd3c32
Stephen Sennett <ssennett@ssennett.net>
2021-11-17 16:08:55 +1100

    Create esc-and-edge.ps1

---
+++ esc-and-edge.ps1
@@ -0,0 +1,12 @@
+# Disable Enhanced Security Configuration
+$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+Stop-Process -Name Explorer
+
+# Install Microsoft Edge
+md -Path $env:temp\edgeinstall -erroraction SilentlyContinue | Out-Null
+$Download = join-path $env:temp\edgeinstall MicrosoftEdgeEnterpriseX64.msi
+Invoke-WebRequest 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/a2662b5b-97d0-4312-8946-598355851b3b/MicrosoftEdgeEnterpriseX64.msi'  -OutFile $Download
+Start-Process "$Download" -ArgumentList "/quiet"
